927e2bc520059dd042a534f418199ec612a8868d
B <77552838+Mrballer59@users.noreply.github.com>
2022-12-01 10:08:57 +0100

    full set up

---
+++ src/lib/PopUp.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./popUp.css";
+
+function PopUp({ setIsOpen }) {
+  return (
+    <>
+      <div className="bigDiv" onClick={() => setIsOpen(false)} />
+      <div className="mainDiv">
+        <div className="modal">
+          <div className="modalContent">Employee Created!</div>
+          <div className="M-Actions">
+            <div className="actionsContainer">
+              <button className="Btn-succ" onClick={() => setIsOpen(false)}>
+                OK
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default PopUp;
